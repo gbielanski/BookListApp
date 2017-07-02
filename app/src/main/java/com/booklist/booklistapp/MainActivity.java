4c8cf424657fc7b73be09b8cff070c5e61798494
Grzegorz Bielański <gbielanski@poczta.onet.pl>
2017-07-02 11:33:11 +0200

    Add AsyncTask

--- app/src/main/java/com/booklist/booklistapp/MainActivity.java
+++ app/src/main/java/com/booklist/booklistapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.booklist.booklistapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BookAsyncTask task = new BookAsyncTask();
+        task.execute();
+    }
+
+    class BookAsyncTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... url) {
+
+            if(url.length == 0 || TextUtils.isEmpty(url[0]))
+                return null;
+
+            return null;
+
+        }
     }
 }
